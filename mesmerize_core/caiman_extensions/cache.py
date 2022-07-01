a40e6c4b634fb01b3e08721e95808a54238be550
Caitlin Lewis <clewis7@email.unc.edu>
2022-06-30 21:03:06 -0400

    updating kwarg comparison to handle numpy arrays

--- mesmerize_core/caiman_extensions/cache.py
+++ mesmerize_core/caiman_extensions/cache.py
@@ -1,6 +1,17 @@
 from functools import wraps
 import pandas as pd
 import time
+import numpy as np
+
+
+def check_kwarg_equality(kwargs, cache_kwargs):
+    if not type(kwargs) == type(cache_kwargs):
+        return False
+
+    if isinstance(cache_kwargs, np.ndarray):
+        return np.array_equal(cache_kwargs, kwargs)
+    else:
+        return cache_kwargs == kwargs
 
 
 class Cache:
@@ -25,12 +36,12 @@ class Cache:
             # if cache is empty, will always be a cache miss
             if len(self.cache.index) == 0:
                 return_val = func(instance, *args, **kwargs)
-                self.cache.loc[len(self.cache.index)] = [instance._series['uuid'], func.__name__, args, kwargs, return_val, time.time()]
+                self.cache.loc[len(self.cache.index)] = [instance._series['uuid'], func.__name__, args, kwargs,
+                                                         return_val, time.time()]
 
             # checking to see if there is a cache hit
             for i in range(len(self.cache.index)):
-                if self.cache.iloc[i, 0] == instance._series['uuid'] and self.cache.iloc[i, 1] == func.__name__ and self.cache.iloc[i, 2] == args and self.cache.iloc[
-                    i, 3] == kwargs:
+                if self.cache.iloc[i, 0] == instance._series['uuid'] and self.cache.iloc[i, 1] == func.__name__  and self.cache.iloc[i, 2] == args and check_kwarg_equality(kwargs, self.cache.iloc[i, 3]):
                     self.cache.iloc[i, 5] = time.time()
                     return_val = self.cache.iloc[i, 4]
                     return self.cache.iloc[i, 4]
@@ -41,12 +52,15 @@ class Cache:
                 self.cache.drop(index=self.cache.sort_values(by=['time_stamp'], ascending=False).index[-1], axis=0,
                                 inplace=True)
                 self.cache = self.cache.reset_index(drop=True)
-                self.cache.loc[len(self.cache.index)] = [instance._series['uuid'], func.__name__, args, kwargs, return_val, time.time()]
+                self.cache.loc[len(self.cache.index)] = [instance._series['uuid'], func.__name__, args, kwargs, return_val,
+                                                         time.time()]
                 return self.cache.iloc[len(self.cache.index) - 1, 4]
             else:
                 return_val = func(instance, *args, **kwargs)
-                self.cache.loc[len(self.cache.index)] = [instance._series['uuid'], func.__name__, args, kwargs, return_val, time.time()]
+                self.cache.loc[len(self.cache.index)] = [instance._series['uuid'], func.__name__, args, kwargs, return_val,
+                                                         time.time()]
 
             return return_val
 
         return _use_cache
+
