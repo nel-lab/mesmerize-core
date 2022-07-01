eb62b656950522866a773031527d87d3cbf4b18f
Caitlin Lewis <clewis7@email.unc.edu>
2022-06-30 20:35:40 -0400

    adding uuid column to cache so that different batch items can be delineated

--- mesmerize_core/caiman_extensions/cache.py
+++ mesmerize_core/caiman_extensions/cache.py
@@ -5,7 +5,7 @@ import time
 
 class Cache:
     def __init__(self, cache_size=10):
-        self.cache = pd.DataFrame(data=None, columns=['function', 'args', 'kwargs', 'return_val', 'time_stamp'])
+        self.cache = pd.DataFrame(data=None, columns=['uuid', 'function', 'args', 'kwargs', 'return_val', 'time_stamp'])
         self.cache_size = cache_size
 
     def get_cache(self):
@@ -22,21 +22,18 @@ class Cache:
         @wraps(func)
         def _use_cache(instance, *args, **kwargs):
 
-            print(instance)
-            print(args, kwargs)
-
             # if cache is empty, will always be a cache miss
             if len(self.cache.index) == 0:
                 return_val = func(instance, *args, **kwargs)
-                self.cache.loc[len(self.cache.index)] = [func.__name__, args, kwargs, return_val, time.time()]
+                self.cache.loc[len(self.cache.index)] = [instance._series['uuid'], func.__name__, args, kwargs, return_val, time.time()]
 
             # checking to see if there is a cache hit
             for i in range(len(self.cache.index)):
-                if self.cache.iloc[i, 0] == func.__name__ and self.cache.iloc[i, 1] == args and self.cache.iloc[
-                    i, 2] == kwargs:
-                    self.cache.iloc[i, 4] = time.time()
-                    return_val = self.cache.iloc[i, 3]
-                    return self.cache.iloc[i, 3]
+                if self.cache.iloc[i, 0] == instance._series['uuid'] and self.cache.iloc[i, 1] == func.__name__ and self.cache.iloc[i, 2] == args and self.cache.iloc[
+                    i, 3] == kwargs:
+                    self.cache.iloc[i, 5] = time.time()
+                    return_val = self.cache.iloc[i, 4]
+                    return self.cache.iloc[i, 4]
 
             # no cache hit, must check cache limit, and if limit is going to be exceeded...remove least recently used and add new entry
             if len(self.cache.index) == self.cache_size:
@@ -44,11 +41,11 @@ class Cache:
                 self.cache.drop(index=self.cache.sort_values(by=['time_stamp'], ascending=False).index[-1], axis=0,
                                 inplace=True)
                 self.cache = self.cache.reset_index(drop=True)
-                self.cache.loc[len(self.cache.index)] = [func.__name__, args, kwargs, return_val, time.time()]
-                return self.cache.iloc[len(self.cache.index) - 1, 3]
+                self.cache.loc[len(self.cache.index)] = [instance._series['uuid'], func.__name__, args, kwargs, return_val, time.time()]
+                return self.cache.iloc[len(self.cache.index) - 1, 4]
             else:
                 return_val = func(instance, *args, **kwargs)
-                self.cache.loc[len(self.cache.index)] = [func.__name__, args, kwargs, return_val, time.time()]
+                self.cache.loc[len(self.cache.index)] = [instance._series['uuid'], func.__name__, args, kwargs, return_val, time.time()]
 
             return return_val
 
