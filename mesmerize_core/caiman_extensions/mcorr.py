ed9a14ca7cb709f731a2d9653b47da8d9cc8e066
kushalkolar <kushalkolar@gmail.com>
2023-01-07 02:12:10 -0500

    more doc and docstring stuff

--- mesmerize_core/caiman_extensions/mcorr.py
+++ mesmerize_core/caiman_extensions/mcorr.py
@@ -56,8 +56,6 @@ class MCorrExtensions:
             |      | read-only.                                                  |
             +------+-------------------------------------------------------------+
 
-
-
         Returns
         -------
         np.ndarray
@@ -66,15 +64,13 @@ class MCorrExtensions:
         Examples
         --------
 
-        View the raw movie and mcorr movie side by side
+        This example visualizes the raw movie and mcorr movie side by side.
+        Needs fastplotlib and must be run in a notebook
 
         .. code-block:: python
 
             from mesmerize_core import load_batch
-
-            # needs fastplotlib and must be run in a notebook
-            from fastplotlib import GridPlot, Image
-            from ipywidgets import IntSlider, VBox
+            from fastplotlib import ImageWidget
 
             df = load_batch("/path/to/batch_dataframe_file.pickle")
 
@@ -82,31 +78,12 @@ class MCorrExtensions:
             input_movie = df.iloc[0].caiman.get_input_movie()
             mcorr_movie = df.iloc[0].mcorr.get_output()
 
-            # gridplot with 1 row 2 columns
-            # synced controllers for pan and zoom
-            gp = GridPlot(shape=(1, 2), controllers="sync")
-
-            # create the graphics for input and mcorr movies
-            input_graphic = Image(input_movie[0], cmap="gnuplot2")
-            mcorr_graphic = Image(mcorr_movie[0], cmap="gnuplot2")
-
-            # add the corresponding graphic to each subplot in the gridplot
-            gp.subplots[0, 0].add_graphic(input_graphic)
-            gp.subplots[0, 1].add_graphic(mcorr_graphic)
-
-            slider = IntSlider(value=0, min=0, max=mcorr_graphic.shape[0] - 1, step=1)
-
-            previous_slider_value = 0
-            def update_frame():  # runs on each rendering cycle
-                if previous_slider_value == slider.value:
-                    return
-
-                input_graphic.update_data(input_movie[slider.value])
-                mcorr_graphic.update_data(mcorr_movie[slider.value])
-
-            gp.add_animdations([update_frame])
-
-            VBox([gp.show(), slider])
+            mcorr_iw = ImageWidget(
+                data=[input_movie, mcorr_movie],
+                vmin_vmax_sliders=True,
+                cmap="gnuplot2"
+            )
+            mcorr_iw.show()
 
         """
         path = self.get_output_path()
