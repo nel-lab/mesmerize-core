d7bfd937616bf89cccac6401fd08daeb2a87fe3e
kushalkolar <kushalkolar@gmail.com>
2023-02-07 23:57:46 -0500

    add LazyVideo

--- mesmerize_core/arrays/__init__.py
+++ mesmerize_core/arrays/__init__.py
@@ -1,9 +1,11 @@
 from ._cnmf import LazyArrayRCM, LazyArrayRCB, LazyArrayResiduals
 from ._tiff import LazyTiff
+from ._video import LazyVideo
 
 __all__ = [
     "LazyArrayRCM",
     "LazyArrayRCB",
     "LazyArrayResiduals",
-    "LazyTiff"
+    "LazyTiff",
+    "LazyVideo"
 ]
