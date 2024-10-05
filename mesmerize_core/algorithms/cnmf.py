fe827b28924aad8b6584c12394ef873ecef8c0f8
Ethan Blackwood <ethanbblackwood@gmail.com>
2024-10-04 22:24:46 -0400

    Get rid of hack for Ain since not needed after last commit

--- mesmerize_core/algorithms/cnmf.py
+++ mesmerize_core/algorithms/cnmf.py
@@ -72,8 +72,6 @@ def run_algo(batch_path, uuid, data_path: str = None, dview=None):
                 Ain = np.load(Ain_path_abs, allow_pickle=True)
                 if Ain.size == 1:  # sparse array loaded as object
                     Ain = Ain.item()
-                # to facilitate parameter comparison
-                cnmf_params.change_params({'init': {'method_init': f'Ain ({params["Ain_path"]})'}})
             else:
                 Ain = None
 
